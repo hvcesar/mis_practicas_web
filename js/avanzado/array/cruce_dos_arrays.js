2f10d89822818519414a5ca145a148b3f5f0a2ba
cesarvegahdez <cesar.vega.hdez@gmail.com>
2020-07-19 02:17:38 -0500

    JS: Corrección de detalles

--- js/avanzado/array/cruce_dos_arrays.js
+++ js/avanzado/array/cruce_dos_arrays.js
@@ -8,24 +8,13 @@ let my_arrary = [
 	{ "id": 4, "name": "Valeria", "year": "26", "flag": 0 }
 ];
 console.log("-------------------------------------");
-console.log("my_arrary");
+console.log("######## my_arrary ########");
 console.log(my_arrary);
 let other_array = [
 	{ "id": 0, "name": "César", "year": "28" },
 	{ "id": 4, "name": "Valeria", "year": "26" }
 ];
 console.log("-------------------------------------");
-console.log("other_array");
+console.log("######## other_array ########");
 console.log(other_array);
-let new_my_array = my_arrary.map( (row, indice, _array) => {
-	let flag_other = false;
-	flag_other = other_array.find( (__other_array) => {
-		return __other_array.id === row.id;
-	});
-	return (flag_other !== undefined)
-		? { "id": row.id, "name": row.name, "year": row.year, "flag": 1 }
-		: { "id": row.id, "name": row.name, "year": row.year, "flag": 0 }
-});
-console.log("-------------------------------------");
-console.log("new_my_array");
-console.log(new_my_array);
+// CONTINUE...
--- js/avanzado/array/map.js
+++ js/avanzado/array/map.js
@@ -1,3 +1,4 @@
+console.log("###############################");
 let my_arrary = [
 	{ "id": 0, "name": "César", "year": "28", "flag": 1 },
 	{ "id": 1, "name": "Ángel", "year": "29", "flag": 0 },
@@ -5,48 +6,30 @@ let my_arrary = [
 	{ "id": 3, "name": "Modesta", "year": "51", "flag": 0 },
 	{ "id": 4, "name": "Valeria", "year": "26", "flag": 1 }
 ];
-
-let new_array = my_arrary.map( (item, id) => {
-    console.log(item);
-    console.log(id);
-    console.log(item.name);
-    return item.flag === 1 ? item.name : "";
-});
-// {id: 0, name: "César", year: "28", flag: 1}
-// 0
-// César
-// {id: 1, name: "Ángel", year: "29", flag: 0}
-// 1
-// Ángel
-// {id: 2, name: "Wendy", year: "22", flag: 0}
-// 2
-// Wendy
-// {id: 3, name: "Modesta", year: "51", flag: 0}
-// 3
-// Modesta
-// {id: 4, name: "Valeria", year: "26", flag: 1}
-// 4
-// Valeria
-// undefined
-// new_array
-// (5) ["César", "", "", "", "Valeria"]
-// 0: "César"
-// 1: ""
-// 2: ""
-// 3: ""
-// 4: "Valeria"
-// length: 5
-// __proto__: Array(0)
-let new_array2 = my_arrary.map( (item, id) => {
+console.log("###############################");
+let new_array = my_arrary.map( (item, id, full_array) => {
+    // console.log("########## item ##########");
+    // console.log(item);
+    // console.log("########## id ##########");
+    // console.log(id);
+    // console.log("########## item.id ##########");
+    // console.log(item.id);
+    // console.log("########## item.name ##########");
+    // console.log(item.name);
+    // console.log("########## array ##########");
+    // console.log(array);
+    // console.log("########## return ##########");
+    // console.log((item.flag === 1));
     return item.flag === 1 
-        ? { "user": item.name, "status": "INSERTADO" } 
-        : { "user": item.name, "status": "ELIMINADO" };
+    ? { "id": item.id, "user": item.name, "status": "TRUE" } 
+    : { "id": item.id, "user": item.name, "status": "FALSE" };
 });
+console.log("###############################");
+console.log("########## new_array ##########");
+console.log(new_array);
 // (5) [{…}, {…}, {…}, {…}, {…}]
-// {user: "César", status: "INSERTADO"}
-// {user: "Ángel", status: "ELIMINADO"}
-// {user: "Wendy", status: "ELIMINADO"}
-// {user: "Modesta", status: "ELIMINADO"}
-// {user: "Valeria", status: "INSERTADO"}
-// length: 5
-// __proto__: Array(0)
+// 0: {id: 0, user: "César", status: "TRUE"}
+// 1: {id: 1, user: "Ángel", status: "FALSE"}
+// 2: {id: 2, user: "Wendy", status: "FALSE"}
+// 3: {id: 3, user: "Modesta", status: "FALSE"}
+// 4: {id: 4, user: "Valeria", status: "TRUE"}
